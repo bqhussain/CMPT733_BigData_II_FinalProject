e15eedf5955f0ddcfffac76efcaa113da189af4b
Kyoun Huh <qyune.h@gmail.com>
2021-04-28 00:09:00 -0700

    remove mimic_app due to app combined

--- code/mimic_app/run.py
+++
@@ -1,6 +0,0 @@
-from web_application import app
-import sys
-
-if __name__ == '__main__':
-    # p = int(sys.argv[1])
-    app.run(host='0.0.0.0', port=5005, threaded=True, debug=False)
--- code/mimic_app/web_application/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask 
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '710b791a8702428c954941af083ad059'
-
-from web_application import routes
-
